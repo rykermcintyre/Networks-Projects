fd0fb0688b781b0f90b15ec23b6b2f5112b3fa50
Ryker Campbell McIntyre <rmcinty3@student06.cse.nd.edu>
2018-10-02 17:34:02 -0400

    set up: done. Started DL

--- program2/client.c
+++ program2/client.c
@@ -1,16 +1,49 @@
+// CLIENT FOR PROGRAMMING ASSIGNMENT 2
+// TCP BASED SIMPLE FTP
+// BASE CODE FROM BEEJ'S GUIDE
+//
+// RYKER McINTYRE rmcinty3
+// THOMAS PLUMMER tplumme2
+// JOSHUA JOHNSON jjohns54
+//
+// October 3, 2018
+
+
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+
+// *********** CHANGE ALL ERROR PRINTFS TO FPRINTF(STDERR,...) IF TIME
+
+// Function primitives
+int DL(char *, int);
+int UP(char *, int);
+int RM(char *, int);
+int LS(char *, int);
+int MKDIR(char *, int);
+int RMDIR(char *, int);
+int CD(char *, int);
 
 int main() {
-	const char* server_name = "localhost";
-	const int server_port = 8877;
+	
+	// ************ CHANGE THIS TO ACCEPT CMD LINE ARGS LATER ************
+	char* server_name = "student06.cse.nd.edu";
+	const int server_port = 41032;
 
 	struct sockaddr_in server_address;
 	memset(&server_address, 0, sizeof(server_address));
 	server_address.sin_family = AF_INET;
+	
+	struct hostent *hp;
+	hp = (struct hostent *)gethostbyname(server_name);
+	bcopy(hp->h_addr, (char *)&server_address.sin_addr, hp->h_length);
 
 	// creates binary representation of server name
 	// and stores it as sin_addr
@@ -29,20 +62,84 @@ int main() {
 
 	// TCP is connection oriented, a reliable connection
 	// **must** be established before any data is exchanged
-	if (connect(sock, (struct sockaddr*)&server_address,
-	            sizeof(server_address)) < 0) {
+	if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
 		printf("could not connect to server\n");
+		printf("%s\n", strerror(errno));
 		return 1;
 	}
+	
+	printf("Connecting to %s on port %d\n", server_name, server_port);
+	printf("Connection established\n");
 
-	// send
+	// sending loop (prompt user for op in client state)
+	while (1) {
+		printf("> ");
+		char cmd[4096];
+		fgets(cmd, 4096, stdin);
+		if (strncmp(cmd, "DL", 2) == 0) {
+			if (DL(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got DL\n");
+		}
+		else if (strncmp(cmd, "UP", 2) == 0) {
+			if (UP(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got UP\n");
+		}
+		else if (strncmp(cmd, "RMDIR", 5) == 0) {
+			if (RMDIR(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got RMDIR\n");
+		}
+		else if (strncmp(cmd, "LS", 2) == 0) {
+			if (LS(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got LS\n");
+		}
+		else if (strncmp(cmd, "MKDIR", 5) == 0) {
+			if (MKDIR(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got MKDIR\n");
+		}
+		else if (strncmp(cmd, "RM", 2) == 0) {
+			if (RM(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got RM\n");
+		}
+		else if (strncmp(cmd, "CD", 2) == 0) {
+			if (CD(cmd, sock) != 0) {
+				fprintf(stderr, "Client command failed\n");
+				return 1;
+			}
+			printf("Got CD\n");
+		}
+		else if (strncmp(cmd, "EXIT", 4) == 0) {
+			printf("Got EXIT\n");
+			if (send(sock, "", 0, 0) < 0) {
+				printf("client send error\n");
+				return 1;
+			}
+			return 0;
+		}
+		else {
+			printf("Please use a valid command\n");
+		}
+	}
 
-	// data that will be sent to the server
-	const char* data_to_send = "Gangadhar Hi Shaktimaan hai";
-	send(sock, data_to_send, strlen(data_to_send), 0);
-
+	/* **** HOW TO RECEIVE BACK FROM SERVER **** 
 	// receive
-
 	int n = 0;
 	int len = 0, maxlen = 100;
 	char buffer[maxlen];
@@ -55,10 +152,129 @@ int main() {
 		len += n;
 
 		buffer[len] = '\0';
-		printf("received: '%s'\n", buffer);
+		printf("client received: '%s'\n", buffer);
 	}
+	*/
 
 	// close the socket
 	close(sock);
 	return 0;
 }
+
+
+// ***************** SIMPLE POPEN EXAMPLE FOR REFERENCE ****************
+/*
+FILE *in = popen("ls -l | grep client | sort", "r");
+char output[4096];
+printf("output is:\n");
+while (fgets(output, sizeof(output), in) > 0) {
+	printf("%s",output);
+}
+*/
+
+
+
+
+
+int DL(char *cmd, int sock) {
+	// Send command to the server
+	if (send(sock, "DL", strlen("DL"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	
+	// break apart command
+	char *dl = strtok(cmd, " ");
+	char *file = strtok(cmd, " ");
+	short int len = htons(strlen(file));
+	
+	// Send length of file name then file name
+	if (send(sock, (char *)&len, sizeof(short int), 0) < 0) {
+		fprintf(stderr, "client send error 1\n");
+		return 1;
+	}
+	if (send(sock, file, len, 0) < 0) {
+		fprintf(stderr, "client send error\n");
+		return 1;
+	}
+	
+	
+	return 0;
+}
+
+
+int UP(char *cmd, int sock) {
+	if (send(sock, "UP", strlen("UP"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	return 0;
+}
+
+
+int RM(char *cmd, int sock) {
+	if (send(sock, "RM", strlen("RM"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	return 0;
+}
+
+
+int LS(char *cmd, int sock) {
+	if (send(sock, "LS", strlen("LS"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	return 0;
+}
+
+
+int MKDIR(char *cmd, int sock) {
+	if (send(sock, "MKDIR", strlen("MKDIR"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	return 0;
+}
+
+
+int RMDIR(char *cmd, int sock) {
+	if (send(sock, "RMDIR", strlen("RMDIR"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	return 0;
+}
+
+
+int CD(char *cmd, int sock) {
+	if (send(sock, "CD", strlen("CD"), 0) < 0) {
+		printf("client send error\n");
+		return 1;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- program2/server.c
+++ program2/server.c
@@ -1,18 +1,44 @@
+// SERVER FOR PROGRAMMING ASSIGNMENT 2
+// TCP BASED SIMPLE FTP
+// BASE CODE FROM BEEJ'S GUIDE
+//
+// RYKER McINTYRE rmcinty3
+// THOMAS PLUMMER tplumme2
+// JOSHUA JOHNSON jjohns54
+//
+// October 3, 2018
+
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 /**
  * TCP Uses 2 types of sockets, the connection socket and the listen socket.
  * The Goal is to separate the connection phase from the data exchange phase.
  * */
 
+int DL(char *, int);
+int UP(char *, int);
+int RM(char *, int);
+int LS(char *, int);
+int MKDIR(char *, int);
+int RMDIR(char *, int);
+int CD(char *, int);
+
+
 int main(int argc, char *argv[]) {
 	// port to start the server on
-	int SERVER_PORT = 8877;
+	// ************* CHANGE TO ACCEPT CMD LINE ARG LATER **************
+	int SERVER_PORT = 41032;
 
 	// socket address used for the server
 	struct sockaddr_in server_address;
@@ -35,8 +61,7 @@ int main(int argc, char *argv[]) {
 
 	// bind it to listen to the incoming connections on the created server
 	// address, will return -1 on error
-	if ((bind(listen_sock, (struct sockaddr *)&server_address,
-	          sizeof(server_address))) < 0) {
+	if ((bind(listen_sock, (struct sockaddr *)&server_address, sizeof(server_address))) < 0) {
 		printf("could not bind socket\n");
 		return 1;
 	}
@@ -51,41 +76,178 @@ int main(int argc, char *argv[]) {
 	// socket address used to store client address
 	struct sockaddr_in client_address;
 	int client_address_len = 0;
+	
+	// Accepting connections message
+	printf("Accepting connections on port %d\n", SERVER_PORT);
 
 	// run indefinitely
 	while (true) {
 		// open a new socket to transmit data per connection
 		int sock;
-		if ((sock =
-		         accept(listen_sock, (struct sockaddr *)&client_address,
-		                &client_address_len)) < 0) {
+		if ((sock = accept(listen_sock, (struct sockaddr *)&client_address, &client_address_len)) < 0) {
 			printf("could not open a socket to accept data\n");
 			return 1;
 		}
 
 		int n = 0;
-		int len = 0, maxlen = 100;
+		int len = 0, maxlen = 4096;
 		char buffer[maxlen];
 		char *pbuffer = buffer;
-
-		printf("client connected with ip address: %s\n",
-		       inet_ntoa(client_address.sin_addr));
+		
+		printf("Connection established\n");
 
 		// keep running as long as the client keeps the connection open
 		while ((n = recv(sock, pbuffer, maxlen, 0)) > 0) {
-			pbuffer += n;
-			maxlen -= n;
-			len += n;
-
-			printf("received: '%s'\n", buffer);
+			// Don't know what the following three lines of code are for,
+			// but they mess it up I think.  Gonna leave them commented for now.
+			//pbuffer += n;
+			//maxlen -= n;
+			//len += n;
+			
+			// The following lines were from a simple server DELETE LATER
+			/*
+			printf("server received: '%s'\n", buffer);
 
 			// echo received content back
-			send(sock, buffer, len, 0);
+			if (send(sock, buffer, len, 0) < 0) {
+				printf("server echo error\n");
+				return 1;
+			}*/
+			if (strncmp(pbuffer, "DL", 2) == 0) {
+				if (DL(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got DL\n");
+			}
+			else if (strncmp(pbuffer, "UP", 2) == 0) {
+				if (UP(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got UP\n");
+			}
+			else if (strncmp(pbuffer, "RMDIR", 5) == 0) {
+				if (RMDIR(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got RMDIR\n");
+			}
+			else if (strncmp(pbuffer, "LS", 2) == 0) {
+				if (LS(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got LS\n");
+			}
+			else if (strncmp(pbuffer, "MKDIR", 5) == 0) {
+				if (MKDIR(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got MKDIR\n");
+			}
+			else if (strncmp(pbuffer, "RM", 2) == 0) {
+				if (RM(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got RM\n");
+			}
+			else if (strncmp(pbuffer, "CD", 2) == 0) {
+				if (CD(pbuffer, sock) != 0) {
+					fprintf(stderr, "Server command handling failed\n");
+					return 1;
+				}
+				printf("Got CD\n");
+			}
+			else {
+				fprintf(stderr, "error in receiving command\n");
+			}
+			
+			memset(pbuffer, '\0', maxlen);
 		}
-
 		close(sock);
 	}
 
 	close(listen_sock);
 	return 0;
 }
+
+int DL(char *cmd, int sock) {
+	
+	// Receive len of filename and filename
+	short int len;
+	if (recv(sock, (char *)&len, sizeof(short int), 0) < 0) {
+		fprintf(stderr, "server recv error\n");
+		return 1;
+	}
+	len = ntohs(len);
+	
+	char file[len];
+	if (recv(sock, file, len, 0) < 0) {
+		fprintf(stderr, "server recv error\n");
+		return 1;
+	}
+	
+	
+	return 0;
+}
+
+
+int UP(char *cmd, int sock) {
+	return 0;
+}
+
+
+int RM(char *cmd, int sock) {
+	return 0;
+}
+
+
+int LS(char *cmd, int sock) {
+	return 0;
+}
+
+
+int MKDIR(char *cmd, int sock) {
+	return 0;
+}
+
+
+int RMDIR(char *cmd, int sock) {
+	return 0;
+}
+
+
+int CD(char *cmd, int sock) {
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
